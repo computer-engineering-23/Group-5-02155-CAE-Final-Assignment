9362b409ec29d3c0ac1f0f5c0f1727ce37c1a1ab
Rasmus Bødker <rasmus.b.bodker@gmail.com>
2025-11-10 13:39:36 +0100

    Add register ABI names

--- src/main.c
+++ src/main.c
@@ -11,7 +11,74 @@
    #define DEBUG_PRINT(fmt, args...) /* Don't do anything on release builds */
 #endif /* ifdef  DEBUG */
 
+enum RV32_REGISTERS {
+   x0 = 0,
+   x1,
+   x2,
+   x3,
+   x4,
+   x5,
+   x6,
+   x7,
+   x8,
+   x9,
+   x10,
+   x11,
+   x12,
+   x13,
+   x14,
+   x15,
+   x16,
+   x17,
+   x18,
+   x19,
+   x20,
+   x21,
+   x22,
+   x23,
+   x24,
+   x25,
+   x26,
+   x27,
+   x28,
+   x29,
+   x30,
+   x31,
 
+   zero = x0,
+   ra = x1,
+   sp = x2,
+   gp = x3,
+   tp = x4,
+   t0 = x5,
+   t1 = x6,
+   t2 = x7,
+   s0 = x8,
+   fp = x8,
+   s1 = x9,
+   a0 = x10,
+   a1 = x11,
+   a2 = x12,
+   a3 = x13,
+   a4 = x14,
+   a5 = x15,
+   a6 = x16,
+   a7 = x17,
+   s2 = x18,
+   s3 = x19,
+   s4 = x20,
+   s5 = x21,
+   s6 = x22,
+   s7 = x23,
+   s8 = x24,
+   s9 = x25,
+   s10 = x26,
+   s11 = x27,
+   t3 = x28,
+   t4 = x29,
+   t5 = x30,
+   t6 = x31,
+};
 
 typedef union {
    uint32_t raw; // Raw 32-bit instruction
@@ -164,16 +231,16 @@ int main(int argc, char *argv[]) {
             break;
 
          case 0x73: // ECALL
-            switch (registers[17]) {
-               case 1: fprintf(stdout, "%d", (int32_t)registers[10]); break; // print_int
-               case 2: fprintf(stdout, "%f", (float)registers[10]); break; // print_float
-               case 4: fprintf(stdout, "%s", (char *)&memory[registers[10]]); break; // print_string
+            switch (registers[a7]) {
+               case 1: fprintf(stdout, "%d", (int32_t)registers[a0]); break; // print_int
+               case 2: fprintf(stdout, "%f", (float)registers[a0]); break; // print_float
+               case 4: fprintf(stdout, "%s", (char *)&memory[registers[a0]]); break; // print_string
                case 10: goto end; break; // exit
-               case 11: fprintf(stdout, "%c", (char)registers[10]); break; // print_char
-               case 34: fprintf(stdout, "0x%02X", registers[10]); break; // print_hex
-               case 35: fprintf(stdout, "0b%08b", registers[10]); break; // print_bin
-               case 36: fprintf(stdout, "%u", registers[10]); break; // print_unsigned
-               case 93: ret = registers[10]; goto end; break; // exit
+               case 11: fprintf(stdout, "%c", (char)registers[a0]); break; // print_char
+               case 34: fprintf(stdout, "0x%02X", registers[a0]); break; // print_hex
+               case 35: fprintf(stdout, "0b%08b", registers[a0]); break; // print_bin
+               case 36: fprintf(stdout, "%u", registers[a0]); break; // print_unsigned
+               case 93: ret = registers[a0]; goto end; break; // exit
             }
             break;
 
@@ -182,7 +249,7 @@ int main(int argc, char *argv[]) {
             break;
       }
 
-      registers[0] = 0;
+      registers[zero] = 0;
 
       for (int i = 0; i < num_registers; i++) {
          DEBUG_PRINT("0x%08X ", registers[i]);
